d6cc23749e65fed2e836359e8a9e721d5b2f0ab2
Techer Florian <techerflorian1@gmail.com>
2025-04-18 12:53:54 +0200

    #1: new stories

---
+++ src/components/Nabvar/navbar.stories.tsx
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import Navbar from "./index";
+// import { FiHome, FiUser, FiSettings } from "react-icons/fi";
+
+const meta: Meta<typeof Navbar> = {
+  title: "UI/Navbar",
+  component: Navbar,
+  tags: ["autodocs"],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Navbar>;
+
+export const BasicNavbar: Story = {
+  args: {
+    navItems: [
+      { path: "/", label: "Home",  },
+      { path: "/profile", label: "Profile",  },
+      { path: "/settings", label: "Settings",  },
+    ],
+  },
+};
